4b320f8f34b56a1badb81ea8e8bc28153a2a80d2
MickaelHm <harmeau.mickael@gmail.com>
2018-12-19 09:51:41 +0100

    Correction

--- Program.cs
+++ Program.cs
@@ -24,6 +24,18 @@ namespace ConsoleApp20
             foreach (string i in Enum.GetNames(typeof(week)))
                 Console.WriteLine(i);
             Console.WriteLine(Enum.GetNames(typeof(week)).GetValue(4));
+            // ou bien Console.WriteLine(Enum.GetNames(typeof(week),4));
+
+            /* correction
+             * Console.WriteLine(week.lundi);
+             * Console.WriteLine(week.mardi);
+             * Console.WriteLine(week.mercredi);
+             * Console.WriteLine(week.jeudi);
+             * Console.WriteLine(week.vendredi);
+             * Console.WriteLine(week.samedi);
+             * Console.WriteLine(week.dimanche);
+             * 
+             * Console.Writeline((week)4);
         }
     }
 }
